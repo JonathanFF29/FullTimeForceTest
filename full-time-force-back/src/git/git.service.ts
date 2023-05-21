df3e813afb3f212f8f4208927d0497f5457dfb89
Jonathan Franco <jonathanf@backbase.com>
2023-05-21 12:25:23 -0500

    git hub api configuration, enviroment variables and axios

--- full-time-force-back/src/git/git.service.ts
+++ full-time-force-back/src/git/git.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+
 
 @Injectable()
 export class GitService {
+  private  apiUrl = process.env.API_URL;
+  private  owner = process.env.GITHUB_OWNER;
+  private repo = process.env.GITHUB_REPO;
 
   async getCommits(): Promise<any[]> {
     try {
-      const commits = ['test'];
-      return commits;
+      const url = `${this.apiUrl}/repos/${this.owner}/${this.repo}/commits`;
+      const response = await axios.get(url);
+      return response.data;
     } catch (error) {
       throw new Error('Error');
     }
--- full-time-force-back/src/main.ts
+++ full-time-force-back/src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AuthController } from './auth/auth.controller';
-import { GitController } from './git/git.controller';
+import { config } from 'dotenv';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
 
-  await app.listen(3000); // Puerto de la aplicación principal
+  await app.listen(3000);
 }
 bootstrap();
