284cbe1a1ef730c428df68fd6e3a02da7e38c987
Jonathan Franco <jonathanf@backbase.com>
2023-05-22 14:15:31 -0500

    adding pagination

--- full-time-force-front/src/app/components/home/home.module.ts
+++ full-time-force-front/src/app/components/home/home.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
-import { GitService } from 'src/app/services/git.service';
 
 
 
