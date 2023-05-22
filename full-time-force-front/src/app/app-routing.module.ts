226e5aed49e8f87767c7b1a29d033e11e70a4211
Jonathan Franco <jonathanf@backbase.com>
2023-05-22 13:15:28 -0500

    guard to protect home route

--- full-time-force-front/src/app/app-routing.module.ts
+++ full-time-force-front/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './util/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -13,6 +14,7 @@ const routes: Routes = [
   },
   {
     path: 'home',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
   },
 ];
